e0b5cbb1a78b6c594071c762eccf090335507ebf
vanilson muhongo <vanilsonmuhongo@gmail.com>
2024-08-26 16:33:15 +0100

    Additional Constants Added
    exception.number_format=Invalid number format: {0}
    exception.illegal_argument=Invalid argument: {0}
    exception.arithmetic=Arithmetic exception occurred.
    exception.null_pointer=Null pointer exception occurred.
    exception.index_out_of_bounds=Index out of bounds: {0}
    exception.unsupported_operation=Unsupported operation.
    exception.io=Input/output error occurred.
    exception.security=Security-related error occurred.

--- src/main/java/code/with/vanilson/common/utils/NumbersUtil.java
+++ src/main/java/code/with/vanilson/common/utils/NumbersUtil.java
@@ -11,6 +11,49 @@ import java.util.List;
  */
 public class NumbersUtil {
 
+    // Integer Constants
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
+    public static final int FIVE = 5;
+    public static final int SIX = 6;
+    public static final int SEVEN = 7;
+    public static final int EIGHT = 8;
+    public static final int NINE = 9;
+    public static final int TEN = 10;
+
+    public static final int ELEVEN = 11;
+    public static final int TWELVE = 12;
+    public static final int THIRTEEN = 13;
+    public static final int FOURTEEN = 14;
+    public static final int FIFTEEN = 15;
+    public static final int SIXTEEN = 16;
+    public static final int SEVENTEEN = 17;
+    public static final int EIGHTEEN = 18;
+    public static final int NINETEEN = 19;
+    public static final int TWENTY = 20;
+
+    // Double Constants
+    public static final double PI = 3.14159265358979323846;
+    public static final double EULER_CONSTANT = 2.71828182845904523536;
+    public static final double GOLDEN_RATIO = 1.61803398874989484820;
+
+    public static final double SQUARE_ROOT_OF_TWO = 1.41421356237309504880;
+    public static final double SQUARE_ROOT_OF_THREE = 1.73205080756887729352;
+    public static final double SQUARE_ROOT_OF_FIVE = 2.23606797749978969641;
+    public static final double SQUARE_ROOT_OF_SEVEN = 2.64575131106459059050;
+    public static final double SQUARE_ROOT_OF_ELEVEN = 3.31662479035539984911;
+
+    // Long Constants
+    public static final long MAX_LONG = Long.MAX_VALUE;
+    public static final long MIN_LONG = Long.MIN_VALUE;
+
+    public static final long BILLION = 1_000_000_000L;
+    public static final long TRILLION = 1_000_000_000_000L;
+    public static final long QUADRILLION = 1_000_000_000_000_000L;
+
     // Constant for milliseconds in a day
     public static final long MILLISECONDS_IN_A_DAY = 1000 * 60 * 60 * 24;
 
@@ -20,6 +63,16 @@ public class NumbersUtil {
 
     // Constants for financial calculations
     public static final double DEFAULT_FINE_RATE_PER_DAY = 2.0; // Euro per day
+
+    // Float Constants
+    public static final float MAX_FLOAT = Float.MAX_VALUE;
+    public static final float MIN_FLOAT = Float.MIN_VALUE;
+
+    public static final float SQUARE_ROOT_OF_TWO_FLOAT = 1.41421356f;
+    public static final float SQUARE_ROOT_OF_THREE_FLOAT = 1.73205081f;
+    public static final float SQUARE_ROOT_OF_FIVE_FLOAT = 2.23606798f;
+    public static final float SQUARE_ROOT_OF_SEVEN_FLOAT = 2.64575131f;
+    public static final float SQUARE_ROOT_OF_ELEVEN_FLOAT = 3.31662479f;
 
     /**
      * Calculates the number of days between two dates.
